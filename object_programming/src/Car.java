b39bcafaf574856b7b2faaf6977a8f43a045df0e
Ola <olapila@o2.pl>
2021-11-20 11:46:29 +0100

    metody start i stop

---
+++ object_programming/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    public void startEngine(){
+        System.out.println("Engine is on");
+    }
+    public void stopEngine(){
+        System.out.println("Engine is off");
+    }
+}
--- object_programming/src/Main.java
+++ object_programming/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car audi = new Car();
+        audi.startEngine();
+        audi.stopEngine();
+
     }
 }
